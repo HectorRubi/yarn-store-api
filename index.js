3ef4b2ee3df8e1fcc577b5dab83602e887fda43e
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-16 14:55:34 -0600

    First example using express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('my app')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('<h1>Nueva ruta</h1>')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  })
+})
+
+app.listen(port, () => {
+  console.log('Listening on http:localhost:' + port);
+})
