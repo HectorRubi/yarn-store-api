34f8d5be0b7267202d28ec20594ed18da0103824
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-17 14:41:54 -0600

    Use put, patch and delete http methods

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -32,9 +32,41 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body
+
   res.json({
     message: 'created',
     data: body
+  })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const body = req.body
+
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  })
+})
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params
+  const body = req.body
+
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  res.json({
+    message: 'deleted',
+    id,
   })
 })
 
