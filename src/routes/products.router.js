7cf671bbae7af7e9acfec447775aca176e1ec531
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-17 16:25:30 -0600

    Handle error of products patch endpoint

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -40,15 +40,21 @@ router.put('/:id', (req, res) => {
 })
 
 router.patch('/:id', async (req, res) => {
-  const { id } = req.params
-  const body = req.body
+  try {
+    const { id } = req.params
+    const body = req.body
 
-  const product = await productService.update(id, body)
-  res.status(200).json({
-    message: 'updated',
-    data: product,
-    id,
-  })
+    const product = await productService.update(id, body)
+    res.status(200).json({
+      message: 'updated',
+      data: product,
+      id,
+    })
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
 })
 
 router.delete('/:id', async (req, res) => {
