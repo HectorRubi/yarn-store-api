3e941fd4e04d312eb233f6210d46d3f69a93f781
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-17 14:22:57 -0600

    Add middleware to allow express recieve data in JSON format

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const routerApi = require('./routes')
 const app = express()
 const port = 3000
 
+// Add middleware to allow express recieve data in JSON format
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>')
 })
