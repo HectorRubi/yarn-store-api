2937210221e0ff4a5b0b57024262d93b57e275db
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-17 17:44:35 -0600

    Add boom library to handle errors and create a boomErrorHandle middleware for that

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routerApi = require('./routes')
 
-const { logErrors, errorHandler } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express()
 const port = 3000
@@ -17,6 +17,7 @@ routerApi(app);
 
 // Middlewares
 app.use(logErrors)
+app.use(boomErrorHandler)
 app.use(errorHandler)
 
 app.listen(port, () => {
--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -13,4 +13,13 @@ function errorHandler(err, req, res, next) {
   })
 }
 
-module.exports = { logErrors, errorHandler }
+function boomErrorHandler(err, req, res, next) {
+  if (err.isBoom) {
+    const { output } = err
+    res.status(output.statusCode).json(output.payload)
+  } else {
+    next(err)
+  }
+}
+
+module.exports = { logErrors, errorHandler, boomErrorHandler }
