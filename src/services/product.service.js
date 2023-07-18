3dd21f494cd957accddd81c3c2d57cfdb068e545
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-17 18:34:15 -0600

    Add conflict errors in products update and delete methods

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -9,11 +9,7 @@ class ProductService {
   }
 
   async find() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.products)
-      }, 5000)
-    })
+    return this.products
   }
 
   async findOne(id) {
@@ -44,23 +40,38 @@ class ProductService {
 
   async update(id, body) {
     const index = this.products.findIndex(product => product.id === id)
+
     if (index === -1) {
       throw boom.notFound('Product not found')
     }
+
     const product = this.products[index]
+
+    if (product.isBlock) {
+      throw boom.conflict('Product is blocked')
+    }
+
     this.products[index] = {
       ...product,
       ...body,
     }
+
     return this.products[index]
   }
 
   async delete(id) {
     const index = this.products.findIndex(product => product.id === id)
+
     if (index === -1) {
       throw boom.notFound('Product not found')
     }
-    const deletedProduct = this.products.splice(index, 1);
+
+    if (this.products[index].isBlock) {
+      throw boom.conflict('Product is blocked')
+    }
+
+    const deletedProduct = this.products.splice(index, 1)
+
     return deletedProduct
 
   }
