69870ebe339f9e9409c3a9ca576f4501e60ddc75
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-18 14:10:33 -0600

    Add user router module and add all user endpoints

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,16 +2,72 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send('No hay parametros')
+router.route('/')
+.get(
+  async (req, res, next) => {
+    try {
+      const users = []
+      res.json(users)
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
+.post(
+  async (req, res, next) => {
+    try {
+      const body = req.body
+      const newProduct = body
+      res.status(201).json({
+        message: 'created',
+        data: newProduct
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+router.route('/:id')
+.get(
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const user = {id}
+      res.json(user)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+.patch(
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const body = req.body
+      const user = {id, body}
+      res.json({
+        message: 'updated',
+        data: user,
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+.delete(
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const user = {id}
+      res.json({
+        message: 'deleted',
+        data: user,
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+)
 
 module.exports = router
