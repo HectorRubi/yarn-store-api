2afe2d0b2a79a8fd2346fecd26c003b45aa9f195
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-21 18:00:27 -0600

    Add .env file, create config module, and use this sensible data from postgres libs

--- src/libs/postgres.js
+++ src/libs/postgres.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg');
 
+const { config } = require('./../config/config');
+
 async function getConnection() {
   const client = new Client({
-    host: 'localhost',
-    port: 5000,
-    user: 'chip',
-    password: 'admin123',
-    database: 'my_store',
+    host: config.dbHost,
+    port: config.dbPort,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
   });
 
   await client.connect();
