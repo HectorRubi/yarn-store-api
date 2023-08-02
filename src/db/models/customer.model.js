8a5c3b4b3883af2664ad2e1a7ba68130ee149729
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-02 09:27:29 -0600

    Add foreing key userId to Customer model as a relation one to one

--- src/db/models/customer.model.js
+++ src/db/models/customer.model.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
+const { USER_TABLE } = require('./user.model');
 
 const CUSTOMER_TABLE = 'customers';
 const CustomerSchema = {
@@ -30,11 +31,22 @@ const CustomerSchema = {
     type: DataTypes.DATE,
     default: Sequelize.NOW,
   },
+  userId: {
+    field: 'user_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: USER_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
 };
 
 class Customer extends Model {
-  static associate() {
-    // define association here
+  static associate(models) {
+    this.belongsTo(models.User, { as: 'user' });
   }
 
   static config(sequelize) {
