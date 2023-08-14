00a36624faf420e3921b4ffe3675c8e73f884593
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-14 13:58:23 -0600

    Add user id field in DB

---
+++ src/db/migrations/20230814193536-add-customer-user-id.js
@@ -0,0 +1,20 @@
+'use strict';
+const {
+  CUSTOMER_TABLE,
+  CustomerSchema,
+} = require('./../models/customer.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.addColumn(
+      CUSTOMER_TABLE,
+      'user_id',
+      CustomerSchema.userId,
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn(CUSTOMER_TABLE, 'user_id');
+  },
+};
