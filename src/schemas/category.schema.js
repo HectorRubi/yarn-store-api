cb8f01c7bad3c2d433cd45cc3d536d0cfe9bac99
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-14 20:28:26 -0600

    Fix category and product schema

--- src/schemas/category.schema.js
+++ src/schemas/category.schema.js
@@ -1,14 +1,17 @@
 const joi = require('joi');
 
 const name = joi.string().min(1).max(50).trim().prefs({ convert: true });
+const image = joi.string().uri();
 
 const createCategorySchema = joi.object({
   name: name.required(),
+  image: image.required(),
 });
 
 const updateCategorySchema = joi
   .object({
     name,
+    image,
   })
   .min(1);
 
--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -1,22 +1,26 @@
 const joi = require('joi');
 
-const id = joi.string().uuid();
+const id = joi.number().integer();
 const name = joi.string().min(3).max(15);
 const price = joi.number().integer().min(10);
 const description = joi.string().min(10);
 const image = joi.string().uri();
+const categoryId = joi.number().integer();
 
 const createProductSchema = joi.object({
   name: name.required(),
   price: price.required(),
   description: description.required(),
   image: image.required(),
+  categoryId: categoryId.required(),
 });
 
 const updateProductSchema = joi.object({
   name,
   price,
   image,
+  description,
+  categoryId,
 });
 
 const idProductSchema = joi.object({
