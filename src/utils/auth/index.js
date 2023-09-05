e9886ea5bd6ddd2b30ddbafedbed56fdf67cc3b5
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-09-05 11:09:06 -0600

    Add Jwt Strategy for login

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -1,8 +1,10 @@
 const passport = require('passport');
 const LocalStrategy = require('./strategies/local.strategy');
+const JwtStrategy = require('./strategies/jwt.strategy');
 
 function passportInit() {
   passport.use(LocalStrategy);
+  passport.use(JwtStrategy);
 }
 
 module.exports = passportInit;
