1cd41aeb5caccb6ca8c15ee9b9ba56b37701bdd4
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-09-05 19:55:10 -0600

    Add mail server env variables

--- src/config/config.js
+++ src/config/config.js
@@ -11,6 +11,10 @@ const config = {
   dbDialect: process.env.DB_DIALECT,
   apiKey: process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
+  mailHost: process.env.MAIL_HOST,
+  mailPort: process.env.MAIL_PORT,
+  mailUser: process.env.MAIL_USER,
+  mailPass: process.env.MAIL_PASS,
 };
 
 module.exports = { config };
